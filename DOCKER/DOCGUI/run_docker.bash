ab093e9b116c2a00b215237dfd0d73a6892bd0b6
kosmicplane <aeroberon@gmail.com>
2025-09-25 13:42:27 +0800

    almost done

--- DOCKER/DOCGUI/run_docker.bash
+++ DOCKER/DOCGUI/run_docker.bash
@@ -1,34 +1,16 @@
 #!/bin/bash
-# Permitir que Docker use tu X11
 xhost +local:root
 
-# Opcional: si usas autenticación X11
-XAUTH=/tmp/.docker.xauth
+export XAUTH=/tmp/.docker.xauth
+export DISPLAY=${DISPLAY:-:0}
 touch $XAUTH
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 
-# Ejecutar el contenedor GUI
 docker run -it --rm \
+    --device /dev/dri:/dev/dri \
     --net=host \
     --env="DISPLAY=$DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    --volume="$XAUTH:$XAUTH" \
+    --volume=$XAUTH:$XAUTH \
     godkitten:latest
-# ==============================
-# ROS 2 + Gazebo en PC GUI
-# ==============================
-
-#  middleware
-export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
-
-# Domin
-export ROS_DOMAIN_ID=42
-
-#(FastDDS)
-export FASTDDS_DEFAULT_PROFILES_FILE=/home/SenDiMoniProg-IMU/DOCKER/DOCGUI/faster_profile.xml
-
-# Gazebo (se conecta al servidor en Jetson)
-export GAZEBO_MASTER_URI=http://192.168.55.1:11345
-export GAZEBO_RESOURCE_PATH=/usr/share/gz
-export GZ_SIM_RESOURCE_PATH=/usr/share/gz
