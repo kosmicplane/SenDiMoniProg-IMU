20f5f6fcd76ed90b5ceced55a3ca9477763ecda0
kosmicplane <aeroberon@gmail.com>
2025-09-25 14:35:23 +0800

    mimu

--- DOCKER/DOCKER_GUI/run_docker.bash
+++ DOCKER/DOCKER_GUI/run_docker.bash
@@ -5,9 +5,13 @@ export XAUTH=/tmp/.docker.xauth
 export DISPLAY=${DISPLAY:-:0}
 touch $XAUTH
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+export XDG_RUNTIME_DIR=/tmp/runtime-$USER
+mkdir -p $XDG_RUNTIME_DIR
+chmod 700 $XDG_RUNTIME_DIR
 
 docker run -it --rm \
-    --device /dev/dri:/dev/dri \
+    --device=/dev/dri/card1 \
+    --device=/dev/dri/renderD128 \
     --net=host \
     --env="DISPLAY=$DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
--- DOCKER/run_docker.bash
+++ DOCKER/run_docker.bash
@@ -15,22 +15,3 @@ docker run -it --rm \
     -p 11346:11346 \
     badkitten:latest
 
-# ==============================
-# ROS 2 + Gazebo en Jetson Nano
-# ==============================
-
-#  middleware
-export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
-
-# Domin
-export ROS_DOMAIN_ID=42
-
-#  (FastDDS)
-export FASTDDS_DEFAULT_PROFILES_FILE=/home/SenDiMoniProg-IMU/DOCKER/DOCGUI/faster_profile.xml
-
-# Gazebo (servidor en Jetson)
-export GAZEBO_MASTER_URI=http://192.168.55.1:11345
-export GAZEBO_RESOURCE_PATH=/usr/share/gz
-export GZ_SIM_RESOURCE_PATH=/usr/share/gz
-
-echo "bad kitten on board"
