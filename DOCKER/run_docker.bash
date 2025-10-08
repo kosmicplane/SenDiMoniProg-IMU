9a813097bf312d7d70573b7cc277c1f4e5fb4cb6
kosmicplane <aeroberon@gmail.com>
2025-10-08 12:22:59 +0800

    tri connect

--- DOCKER/run_docker.bash
+++ DOCKER/run_docker.bash
@@ -1,9 +1,7 @@
 xhost local:root
 #docker-compose -f docker-compose.yml up -d --build
-
 XAUTH=/tmp/.docker.xauth
-export DISPLAY=:0
-xhost +local:root
+export DISPLAY=${DISPLAY:-:0}
 docker run -it --rm \
     --device=/dev/rfcomm0:/dev/rfcomm0 \
     --net=host \
