826e75c7e84384585fc6f870176664d014555de4
kosmicplane <aeroberon@gmail.com>
2025-10-27 14:30:58 +0800

    restructure

--- DOCKER/DOCKER_base/initilize.bash
+++ DOCKER/DOCKER_base/initilize.bash
@@ -2,9 +2,6 @@
 # Create an entrypoint script that initializes ROS2 and runs your node
 # ---------------------------------------------------------
 RUN echo '#!/bin/bash\n\
-echo "🚀 Starting ROS2 environment..."\n\
-source /opt/ros/kilted/setup.bash\n\
-cd /home/SenDiMoniProg-IMU\n\
 git pull\n\
 cd ROS2_PACKAGES/imu_bt_publisher/imu_bt_publisher\n\
 python3 imu_publisher.py\n\
