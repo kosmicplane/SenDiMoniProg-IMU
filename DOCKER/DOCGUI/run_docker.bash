b804c8c5448f887a0b5df2af2c190575e8b0bf58
kosmicplane <aeroberon@gmail.com>
2025-10-27 17:58:54 +0800

    past

--- DOCKER/DOCGUI/run_docker.bash
+++ DOCKER/DOCGUI/run_docker.bash
@@ -1,20 +1,24 @@
-#!/bin/bash
-xhost +local:root
-
-export XAUTH=/tmp/.docker.xauth
+xhost local:root
+#docker-compose -f docker-compose.yml up -d --build
+XAUTH=/tmp/.docker.xauth
 export DISPLAY=${DISPLAY:-:0}
-touch $XAUTH
-xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
-export XDG_RUNTIME_DIR=/tmp/runtime-$USER
-mkdir -p $XDG_RUNTIME_DIR
-chmod 700 $XDG_RUNTIME_DIR
+#!/bin/bash
+
+USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
+PROJECT_DIR="$USER_HOME/Desktop/SenDiMoniProg-IMU"
+
+cd "$PROJECT_DIR"
+echo "🔄 Updating repository before mounting..."
+git pull origin main || git pull origin master
+echo "✅ Repository updated."
 
 docker run -it --rm \
+    --device=/dev/rfcomm0:/dev/rfcomm0 \
     --net=host \
-    --device=/dev/dri:/dev/dri \
     --env="DISPLAY=$DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    --volume=$XAUTH:$XAUTH \
-    --volume /usr/lib/x86_64-linux-gnu/dri:/usr/lib/x86_64-linux-gnu/dri:ro \
-    godkitten:latest gz gui
+    --volume="/home/SenDiMoniProg-IMU/ROS2_PACKAGES:/root/ros2_ws/src" \
+    --volume="$XAUTH:$XAUTH" \
+    --privileged \
+    badkitten:latest
